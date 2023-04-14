e98af05c73286a27409787e0a2be4fd929b3e84d
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-04-14 14:31:01 -0400

    Deploy menu open tweaks

--- code/game.js
+++ code/game.js
@@ -215,7 +215,7 @@ scene('game', () => {
     pos(SCALE*1.25, SCALE*(5.5 - 1/8)),
     // from 1.25 to 6.25 (0->5 +1.25)
     anchor('center'),
-    scale(1.5),
+    scale(1),
     color(WHITE),
     z(Z.ui),
     fixed(),
@@ -227,13 +227,13 @@ scene('game', () => {
   ]);
 
   const deployMainText = add([
-    text('Deploying', {size: SCALE*0.4}),
+    text('Deploying 1', {size: SCALE*0.4}),
     pos(SCALE*0.75, SCALE*4.5),
     fixed(),
     z(Z.ui),
     "helperSpawnUI",
     {
-      ogY: SCALE*5.5,
+      ogY: SCALE*4.5,
     }
   ]);
 
