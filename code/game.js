18a64fb7cd3634be5494cfa00570d0d0543202e1
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-05-04 09:43:41 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -152,7 +152,7 @@ scene('game', () => {
   // helper spawn ui
 
   console.log(deploy);
-  import * as deploy from '_deployMenu.js';
+  //import * as deploy from '_deployMenu.js';
 
   // fighting functions
 
