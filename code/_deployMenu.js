d1243f5bbac0b8c015f41468e7e670619e33a8b9
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-05-08 06:10:13 -0400

    Update _deployMenu.js

--- code/_deployMenu.js
+++ code/_deployMenu.js
@@ -1,3 +1,9 @@
+let closeSpawnUI;
+let deploySetBar;
+let delpoyCountDrag;
+let deployMainText;
+    
+
 function menu_deploy() {
   const deploy = add([pos(0,0), rect(0,0)]);
 
@@ -14,7 +20,7 @@ function menu_deploy() {
     }
   ]);
 
-  const closeSpawnUI = deploy.add([
+  closeSpawnUI = deploy.add([
     rect(SCALE/2, SCALE/2),
     pos(SCALE, height() - SCALE*2),
     anchor('center'),
@@ -29,7 +35,7 @@ function menu_deploy() {
     }
   ]);
 
-  const deploySetBar = deploy.add([
+  deploySetBar = deploy.add([
     rect(SCALE*6.125, SCALE/4),
     pos(SCALE*0.75, SCALE*5.25),
     color(rgb(100,100,100)),
@@ -45,7 +51,7 @@ function menu_deploy() {
     }
   ]);
 
-  const deployCountDrag = deploy.add([
+  deployCountDrag = deploy.add([
     rect(SCALE*0.5, SCALE*0.5),
     pos(SCALE*1.25, SCALE*(5.5 - 1/8)),
     // from 1.25 to 6.25 (0->5 +1.25)
@@ -61,7 +67,7 @@ function menu_deploy() {
     }
   ]);
 
-  const deployMainText = deploy.add([
+  deployMainText = deploy.add([
     text('Deploying 1', {size: SCALE*0.4}),
     pos(SCALE*0.75, SCALE*4.5),
     fixed(),
